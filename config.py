c3b618978f5aaf97ed848add51898bb6f17814a8
emovies604-ux <emovies604@gmail.com>
2025-08-31 21:35:00 +0530

    Update config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+API_ID = 27020363               # Replace with your actual API ID
+API_HASH = "900133bfe09ce6ef78885e3599ba64ca"         # Replace with your actual API Hash
+BOT_TOKEN = ""       # Replace with your actual Bot Token from BotFather
+MONGO_URI = "mongodb+srv://Billobb:Billobb@billobb.v67fmki.mongodb.net/?retryWrites=true&w=majority&appName=billobb" # Replace with your MongoDB Atlas connection string
+
+# Optional: Admin IDs for managing the bot (replace with your Telegram User IDs)
+# You can find your User ID by forwarding any message to @userinfobot or @missrose_bot
+ADMINS = [1222287481]
